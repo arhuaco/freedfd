35c01f07e1e1fbbf9d62f906f709ed67c9fa5b3f
nelsoneci <nelsoneci@df3bcf99-0e20-0410-8016-bbfc507d3ee1>
2008-04-16 17:54:54 +0000

    Para traducciones

---
+++ deprecated-owl/src/procesa-menu.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+open MSG, '<messages.h';
+
+my %S;
+
+my $lang = '__SPANISH__';
+my $is_in = 0;
+
+while(<MSG>)
+{
+  if (/$lang/)
+  {
+    $is_in = 1 - $is_in
+  }
+  for my $prefix ('TXT_CM_', 'TXT_MENU_', 'HELP_CM_')
+  {
+    if ($is_in && /define\s+($prefix\w+)\s+(\".*?\")/)
+    {
+      $S{$1} = $2;
+    }
+  }
+}
+
+close MSG;
+
+open RC, '<dfd.rc.source';
+
+while (<RC>)
+{
+  my $orig = $_;
+  foreach my $key (keys %S)
+  {
+    if ($orig =~ m/$key/)
+    {
+      $orig =~ s/$key/$S{$key}/;
+    }
+  }
+  print $orig;
+}
