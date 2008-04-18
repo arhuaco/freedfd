afdf93c47b2757e2066191faa90fe6fa1691c3f0
nelsoneci <nelsoneci@df3bcf99-0e20-0410-8016-bbfc507d3ee1>
2008-04-18 00:53:21 +0000

    Entorno de ejecucion: pila de tablas

---
+++ trunk/src/entorno-ejecucion.h
@@ -0,0 +1,53 @@
+#ifndef __ENTORNO_EJECUCION_H__
+#define __ENTORNO_EJECUCION_H__
+
+class PilaDeTablas
+{
+
+  Tabla *Tope;
+
+public:
+    PilaDeTablas ()
+  {
+    Tope = 0;
+  }
+  void Apilar (Tabla * UnaTabla)
+  {
+    UnaTabla->SetSig (Tope);
+    Tope = UnaTabla;
+  }
+  void Desapilar ()
+  {
+    Tabla *Aux;
+    Aux = Tope;
+    Tope = Tope->GetSig ();
+    delete Aux;
+  }
+
+  Variable *Crear (char *Id, Token * UnToken, Variable * Vengo,
+                   unsigned *Indices, int Dim)
+  {
+    return Tope->Crear (Id, UnToken, Vengo, Indices, Dim);
+  }
+  Variable *Crear (char *Id, Variable * Vengo, bool FP)
+  {
+    return Tope->Crear (Id, Vengo, FP);
+  }
+  Variable *Buscar (char *Id);
+  void AsignarValor (char *Id, Token * UnToken, unsigned *Indices, int Dim)
+  {
+
+    Tope->AsignarValor (Id, UnToken, Indices, Dim);
+    return;
+  }
+  Tabla *GetTope ()
+  {
+    return Tope;
+  }
+  Token *Leer (char *Id, unsigned *Indices, int Dim);
+  void ActualizarVariables (Variable *);
+  void Vacear ();
+
+} PilaDeTablas;
+
+#endif
