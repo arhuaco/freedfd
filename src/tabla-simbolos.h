8b651c199acb281226e201b0a14b6c540446990e
nelsoneci <nelsoneci@df3bcf99-0e20-0410-8016-bbfc507d3ee1>
2008-04-18 00:55:48 +0000

    Ahora tenemos tabla de simbolos.

---
+++ src/tabla-simbolos.h
@@ -0,0 +1,46 @@
+#ifndef __TABLA__SIMBOLOS__H__
+#define __TABLA__SIMBOLOS__H__
+
+class Tabla
+{
+  // ObjetoEjecucion *Retorno;
+  Variable *Inicio;
+  Tabla *Sig;
+
+public:
+    Tabla ()
+  {
+    Inicio = 0;
+    ContadorTabla++;
+  }
+  Variable *Crear (char *Id, Token * Untoken, Variable * Vengo,
+                   unsigned *Indices, int Dim);
+  Variable *Crear (char *Id, Variable * Vengo, bool FP);
+  Variable *Buscar (char *Id);
+  Variable *GetInicio ()
+  {
+    return Inicio;
+  }
+  Token *Leer (char *Id, unsigned *Indices, int Dim);
+  void AsignarValor (char *Id, Token * UnToken, unsigned *Indices, int Dim);
+  /*void SetRetorno (ObjetoEjecucion * R)
+  {
+    Retorno = R;
+  }
+  ObjetoEjecucion *GetRetorno ()
+  {
+    return Retorno;
+  }*/
+  void SetSig (Tabla * UnSig)
+  {
+    Sig = UnSig;
+  }
+  Tabla *GetSig ()
+  {
+    return Sig;
+  }
+  ~Tabla ();
+
+};
+
+#endif
