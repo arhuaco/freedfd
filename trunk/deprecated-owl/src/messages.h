930789ec0067dbfa3de1156a7a6d0f5bd3947906
nelsoneci <nelsoneci@df3bcf99-0e20-0410-8016-bbfc507d3ee1>
2008-04-22 03:27:54 +0000

    added english

--- trunk/deprecated-owl/src/messages.h
+++ trunk/deprecated-owl/src/messages.h
@@ -6,8 +6,8 @@
  *
  */
 
-#if 1 /* Español -- Idioma por defecto */
-  /* __SPANISH__ BEGIN */
+#if 1 /* English */
+  /* __ENGLISH__ BEGIN */
   #define TXT_DESBORDAMIENTO "Desbordamiento en la operación"
   #define TXT_DESBORDAMIENTO_NEGATIVO "Desbordamiento negativo en la operación"
   #define TXT_LOS_TIPOS_NO_COINCIDEN "Los tipos no coinciden"
@@ -228,9 +228,255 @@
   #define TXT_FREEDFD_LICENSE "FreeDFD es Software Libre, y está cubierto por la licencia  pública GNU (GPL).\n\nUsted puede tener acceso al código fuente, y puede modificarlo de acuerdo a sus necesidades si cumple con la licencia. Lo invitamos a copiar y distribuir este programa.\n\nEstamos trabajando en una nueva versión. Necesitamos ayuda para el desarrollo. Favor visitar la página (http://freedfd.freaks-unidos.net) para obtener más información sobre el desarrollo de la nueva version"
   #define TXT_WEBPAGE_CHECK "Consulte la página web para ver la lista completa de autores y colaboradores"
 
+  /* __ENGLISH__ END */
+
+
+
+
+
+
+#elif 0 /* Español */
+  /* __SPANISH__ BEGIN */
+
+
+
+
+
+  #define TXT_DESBORDAMIENTO "Desbordamiento en la operación"
+  #define TXT_DESBORDAMIENTO_NEGATIVO "Desbordamiento negativo en la operación"
+  #define TXT_LOS_TIPOS_NO_COINCIDEN "Los tipos no coinciden"
+  #define TXT_ID_NO_ES_ARREGLO "El identificador no es es un arreglo"
+  #define TXT_DIMENSION_NO_VALIDA "Dimensión no válida"
+  #define TXT_CONSTANTE_NUMERICA_ILEGAL "Constante numérica ilegal"
+  #define TXT_NOMBRE_VARIABLE_INCORRECTO "Nombre de variable incorrecto"
+  #define TXT_FUNCION_REQUIERE_PARAMETRO "La función requiere argumentos"
+  #define TXT_FALTA_OPERADOR "Se espera un operador"
+  #define TXT_CADENA_INDETERMINADA "Se espera ' para delimitar el fin de la cadena"
+  #define TXT_CONSTANTE_LOGICA_ILEGAL "Se esperaba una constante lógica( .V. o .F. )"
+  #define TXT_OPERADOR_LOGICO_ILEGAL "Operador lógico ilegal"
+  #define TXT_FALTA_OPERANDO "Se esperaba un operando"
+  #define TXT_OPERADOR_ILEGAL "Operador no válido"
+  #define TXT_CARACTER_ILEGAL "Caracter no válido"
+  #define TXT_CERRADO_PARENTESIS_NO_ABIERTO "Cerrado paréntesis no abierto"
+  #define TXT_ABIERTO_PARENTESIS_NO_CERRADO "Abierto paréntesis no cerrado"
+  #define TXT_COMA_ILEGAL "Uso ilegal del operador"
+  #define TXT_ERROR_MATEMATICO "Error Matemático"
+  #define TXT_LLAMADO_DE_FUNCION_NO_VALIDO "Llamado de función no válido"
+  #define TXT_VARIABLE_NO_INICIALIZADA "Variable no inicializada"
+  #define TXT_VARIABLE_NO_EXISTE "Variable no existe"
+  #define TXT_DIVISION_POR_CERO "División por cero"
+  #define TXT_SUBINDICE_NO_VALIDO ": Indice no válido"
+  #define TXT_INDICES "Los índices deben ser enteros no negativos menores o iguales a 65535."
+  #define TXT_ILEGAL_COMA_O_CADENA_VACIA "Uso ilegal de la coma ó falta expresión"
+  #define TXT_SOLO_VARIABLES "La asignación de valores solo se realiza en campos variables"
+  #define TXT_NO_INDICES "No hay índices entre los paréntesis. Los paréntesis se usan para acceder a elementos de un arreglo."
+  #define TXT_TIPO_DEBE_SER_LOGICO "La condición debe ser de tipo lógico"
+  #define TXT_VINICIAL_CONTADOR "Se necesita un valor inicial para el contador"
+  #define TXT_VFINAL_CONTADOR "Se necesita un valor final para el contador"
+  #define TXT_INCREMENTO_CONSTANTE "El incremento debe ser una constante real"
+  #define TXT_CONTADOR_REAL "El valor del contador debe ser de tipo real"
+  #define TXT_LIMITE_REAL "El valor del límite para el contador debe ser de tipo real"
+  #define TXT_PARAMETRO_NO_VARIABLE "Los parámetros deben ser campos variables"
+  #define TXT_ILEGAL_NOMBRE_SUBPROGRAMA "Nombre no válido para un subprograma"
+  #define TXT_PARAMETRO_REPETIDO "Parámetro Repetido"
+  #define TXT_SUBPROGRAMA_NO_EXISTE ": No se encontró el Subprograma"
+  #define TXT_SUBPROGRAMA_REPETIDO ": Subprograma repetido"
+  #define TXT_MUCHOS_ARGUMENTOS ": Demasiados argumentos"
+  #define TXT_POCOS_ARGUMENTOS ": Muy pocos argumentos"
+  #define TXT_ERROR_INDEFINIDO ": Error no definido"
+
+  /* menú */
+
+  #define TXT_MENU_FILE "&Archivo"
+  #define TXT_MENU_EDIT "&Edicion"
+  #define TXT_MENU_OBJECT "&Objeto"
+  #define TXT_MENU_VIEW "&Ver"
+  #define TXT_MENU_ZOOM "&Zoom"
+  #define TXT_MENU_RUN "Eje&cución"
+  #define TXT_MENU_DEBUG "&Depuración"
+  #define TXT_MENU_THEOPTIONS "O&pciones"
+  #define TXT_MENU_THEHELP "A&yuda"
+
+  #define TXT_CM_ARCHIVONUEVO "&Nuevo\tCtrl+N"
+  #define TXT_CM_ARCHIVOABRIR "&Abrir...\tCtrl+A"
+  #define TXT_CM_ARCHIVOGUARDAR "&Guardar\tCtrl+G"
+  #define TXT_CM_ARCHIVOGUARDARCOMO "Guardar &Como...     "
+  #define TXT_CM_ARCHIVOCONFIGURARIMPRESORA "Configurar impresora..."
+  #define TXT_CM_ARCHIVOIMPRIMIR "Im&primir...\tCtrl+P"
+  #define TXT_CM_ARCHIVOSALIR "&Salir\tAlt+F4"
+  #define TXT_CM_EDICIONCORTAR "&Cortar\tCtrl+X"
+  #define TXT_CM_EDICIONCOPIAR "C&opiar\tCtrl+C"
+  #define TXT_CM_EDICIONPEGAR "&Pegar\tCtrl+V"
+  #define TXT_CM_EDICIONELIMINAR "&Eliminar\tSupr"
+  #define TXT_CM_EDICIONELIMINARSUBPROGRAMA "Eliminar &Subprograma"
+  #define TXT_CM_EDICIONINSERTAROBJETO "Insertar Objeto\tIns"
+  #define TXT_CM_EDICIONEDITAR "E&ditar Objeto...\tEnter"
+  #define TXT_CM_SELECCIONACURSORMENU "&Cursor"
+  #define TXT_CM_OBJETOASIGNACIONMENU "&Asignacion"
+  #define TXT_CM_OBJETOMIENTRASMENU "Ciclo &mientras"
+  #define TXT_CM_OBJETOPARAMENU "Ciclo &para"
+  #define TXT_CM_OBJETODECISIONMENU "&Decision"
+  #define TXT_CM_OBJETOLECTURAMENU "L&ectura"
+  #define TXT_CM_OBJETOLLAMADAMENU "&Llamada"
+  #define TXT_CM_OBJETOSALIDAMENU "&Salida"
+  #define TXT_CM_OBJETONUEVOSUBPROGRAMA "&Nuevo Subprograma"
+  #define TXT_CM_VERZOOMMAS "&Acercar\t+        "
+  #define TXT_CM_VERZOOMMENOS "A&lejar\t-         "
+  #define TXT_CM_VERSUBPROGRAMAMENOS "An&terior subprograma\tCtrl+Izquierda"
+  #define TXT_CM_VERSUBPROGRAMAMAS "&Siguiente Subprograma\tCtrl+Derecha"
+  #define TXT_CM_VERDEPURADOR "&Depurador\tF6 "
+  #define TXT_CM_DEPURAREJECUTAR "&Ejecutar\tF9 "
+  #define TXT_CM_DEPURARPAUSAR "P&ausar\tPausa"
+  #define TXT_CM_DEPURARDETENER "&Detener\tCtrl+Pausa"
+  #define TXT_CM_DEPURARPASOSIMPLE "&Paso simple\tF7"
+  #define TXT_CM_DEPURAREJECUTARHASTA "&Ejecutar hasta\tF4"
+  #define TXT_CM_DEPURAREVALUAR "E&valuar\tF5"
+  #define TXT_CM_ANGULOSENGRADOS "&Angulos en &grados"
+  #define TXT_CM_ANGULOSENRADIANES "Angulos en &radianes"
+  #define TXT_CM_AYUDACONTENIDO "&Contenido...\tF1"
+  #define TXT_CM_AYUDAACERCADE "&Acerca de...     "
+
+  /* menú para depurar expresiones */
+
+  #define TXT_CM_DEBUG_MENUTITLE "&Edición de expresiones a observar"
+  #define TXT_CM_EDICIONNUEVO  "&Nueva\tIns"
+  #define TXT_CM_EDICIONEDITARDEPURADOR "E&ditar\tEnter"
+  #define TXT_CM_EDICIONELIMINARDEPURADOR "Eli&minar\tSupr"
+
+  /* Ayuda contextual */
+
+  #define HELP_CM_ARCHIVONUEVO "Inicia la sesión de trabajo con un nuevo diagrama"
+  #define HELP_CM_ARCHIVOABRIR "Inicia la sesión de trabajo con un diagrama ya existente"
+  #define HELP_CM_ARCHIVOGUARDAR "Guarda el diagrama en edición"
+  #define HELP_CM_ARCHIVOGUARDARCOMO "Guarda el diagrama en edición con un nuevo nombre"
+  #define HELP_CM_VERZOOMMENOS "Dismunuye el tamaño de los objetos en pantalla"
+  #define HELP_CM_VERZOOMMAS "Aumenta el tamaño de los objetos en pantalla"
+  #define HELP_CM_ARCHIVOSALIR "Salir de Creación"
+  #define HELP_CM_AYUDAACERCADE "Muestra información sobre los autores y la versión"
+  #define HELP_CM_OBJETOASIGNACION "Permite asignar valores a  variables"
+  #define HELP_CM_OBJETOMIENTRAS "Ejecuta un bloque de instrucciones mientras la condición sea verdadera"
+  #define HELP_CM_OBJETOPARA "Ejecuta un bloque de instrucciones hasta que el contador alcance el límite"
+  #define HELP_CM_OBJETODECISION "Selecciona el flujo a seguir de acuerdo al valor lógico de una condición"
+  #define HELP_CM_OBJETOLECTURA "Permite dar valores a las variables desde teclado"
+  #define HELP_CM_OBJETOLLAMADA "Invoca a un subprograma"
+  #define HELP_CM_OBJETOSALIDA "Muestra valores por pantalla"
+  #define HELP_CM_OBJETONUEVOSUBPROGRAMA "Crea un nuevo subprograma"
+  #define HELP_CM_SELECCIONACURSOR "Permite seleccionar objetos del diagrama en edición"
+  #define HELP_CM_EDICIONELIMINARDEPURADOR "Elimina la expresión seleccionado"
+  #define HELP_CM_EDICIONCORTAR "Corta el objeto seleccionado"
+  #define HELP_CM_EDICIONCOPIAR "Copia el objeto seleccionado"
+  #define HELP_CM_EDICIONPEGAR "Inserta a continuación del objeto seleccionado"
+  #define HELP_CM_VERSUBPROGRAMAMAS "Visualiza el siguiente subprograma "
+  #define HELP_CM_VERSUBPROGRAMAMENOS "Visualiza el subprograma anterior"
+  #define HELP_CM_EDICIONELIMINARSUBPROGRAMA "Elimina el subprograma en edición"
+  #define HELP_CM_DEPURAREJECUTAR "Ejecuta el algoritmo representado por el diagrama en edición"
+  #define HELP_CM_DEPURARDETENER "Termina la ejecución del algoritmo"
+  #define HELP_CM_DEPURARPAUSAR "Realiza una pausa en la ejecución del algoritmo"
+  #define HELP_CM_DEPURARPASOSIMPLE "Ejecuta el siguiente objeto"
+  #define HELP_CM_DEPURAREJECUTARHASTA "Ejecuta el algoritmo deteniéndose en el objeto seleccionado"
+  #define HELP_CM_ANGULOSENGRADOS "Angulos en grados para las funciones trigonométricas"
+  #define HELP_CM_ANGULOSENRADIANES "Angulos en radianes para las funciones trionométricas"
+  #define HELP_CM_EDICIONINSERTAROBJETO "Inserta el objeto indicado por el puntero a continuación del objeto seleccionado"
+  #define HELP_CM_EDICIONEDITARDEPURADOR "Edita la expresión seleccionada"
+  #define HELP_CM_VERDEPURADOR "Muestra o esconde la ventana de depuración"
+  #define HELP_CM_VERDEPURADORBOTON "Muestra o esconde la ventana de depuración"
+  #define HELP_CM_EDICIONNUEVO "Inserta una nueva expresión"
+  #define HELP_CM_OBJETOASIGNACIONMENU "Permite asignar valores a  variables"
+  #define HELP_CM_OBJETOMIENTRASMENU "Ejecuta un bloque de instrucciones mientras la condición sea verdadera"
+  #define HELP_CM_OBJETOPARAMENU "Ejecuta un bloque de instrucciones hasta que el contador alcance el límite"
+  #define HELP_CM_OBJETODECISIONMENU "Selecciona el flujo a seguir de acuerdo al valor lógico de una condición"
+  #define HELP_CM_OBJETOLECTURAMENU "Permite dar valores a las variables desde teclado"
+  #define HELP_CM_OBJETOLLAMADAMENU "Invoca a un subprograma"
+  #define HELP_CM_OBJETOSALIDAMENU "Muestra valores por pantalla"
+  #define HELP_CM_SELECCIONACURSORMENU "Permite seleccionar objetos del diagrama en edición"
+  #define HELP_CM_EDICIONELIMINAR "Elimina del diagrama el objeto seleccionado"
+  #define HELP_CM_EDICIONEDITAR "Edita el contenido del objeto seleccionado"
+  #define HELP_CM_DEPURAREVALUAR "Muestra el diálogo para evaluación de expresiones"
+  #define HELP_CM_AYUDACONTENIDO "Muestra el contenido de la ayuda"
+  #define HELP_CM_ARCHIVOIMPRIMIR "Imprime la hoja actual"
+  #define HELP_CM_ARCHIVOCONFIGURARIMPRESORA "Configura las características relacionadas con la impresora"
+
+  /* Dialogs, message boxes */
+
+  #define TXT_DIALOG_EXECUTION_OK "La ejecución ha terminado con normalidad"
+  #define TXT_DIALOG_REPORT_BUG "Favor informar a los autores para que arreglen el error"
+  #define TXT_DIALOG_CONFIRM_DELETE_SUBPROGRAM1  "Si elimina este objeto eliminará el subprograma completo.\n¿Eliminar?"
+  #define TXT_DIALOG_CONFIRM_DELETE_SUBPROGRAM2 "¿Eliminar el subprograma?"
+  #define TXT_DIALOG_CONFIRM_DELETE_BOTH_BRANCHES "Si elimina este objeto eliminará también su brazo izquierdo y su brazo derecho.\n¿Eliminar? "
+  #define TXT_DIALOG_CONFIRM_DELETE_LOOP "Si elimina este objeto eliminará también el cuerpo del ciclo.\n¿Eliminar?"
+  #define TXT_DIALOG_ADD_CONSTANT "Debe ingresar un valor constante"
+  #define TXT_DIALOG_EVALUATION_ERROR "Error de evaluación"
+  #define TXT_DIALOG_UNABLE_SAVE_FILE "No fue posible guardar el archivo"
+  #define TXT_DIALOG_UNABLE_OPEN_FILE "No fue posible abrir el archivo"
+  #define TXT_DIALOG_INVALID_FILE_FORMAT "El archivo no es un archivo de Dfd"
+  #define TXT_DIALOG_PROGRAM_CHECK_ERROR "Error de revisión"
+  #define TXT_DIALOG_PROGRAM_RUN_ERROR "Error de revisión"
+
+  /* Objetos en Ejecución */
+
+  #define TXT_RUNNING_OBJECT_YES "Sí"
+  #define TXT_RUNNING_OBJECT_NO "No"
+  #define TXT_RUNNING_OBJECT_FOR "Para"
+  #define TXT_RUNNING_OBJECT_WHILE "MQ"
+  #define TXT_RUNNING_OBJECT_BEGIN "Inicio"
+  #define TXT_RUNNING_OBJECT_END "Fin"
+  #define TXT_RUNNING_OBJECT_BLOCK_END "Cierre"
+  #define TXT_RUNNING_OBJECT_RETURN "Retornar"
+
+  /* Cuadros de diálogo */
+
+  #define TXT_DIALOG_CLOSE "Cerrar"
+  #define TXT_DIALOG_EVALUATE "Evaluar"
+  #define TXT_DIALOG_EXPRESSION "Expresión:"
+  #define TXT_DIALOG_NEW_EXPRESSION "Nueva expresión"
+  #define TXT_DIALOG_RESULT "Resultado:"
+  #define TXT_DIALOG_OK "Aceptar"
+  #define TXT_DIALOG_CANCEL "Cancelar"
+  #define TXT_DIALOG_LEFT "Izquierda"
+  #define TXT_DIALOG_RIGHT "Derecha"
+  #define TXT_DIALOG_INSERT_AT "¿ Insertar en ... ?"
+  #define TXT_DIALOG_PAUSE "Pausa"
+  #define TXT_DIALOG_CONTINUE "Continuar"
+  #define TXT_DIALOG_OUTPUT_TITLE "Salida por pantalla"
+  #define TXT_DIALOG_OUTPUT "Salida:"
+  #define TXT_DIALOG_INPUT_TITLE "Entrada de valores por teclado"
+  #define TXT_DIALOG_INPUT_VALUE "Valor a ingresar:"
+  #define TXT_DIALOG_INPUT_VALUE_TO_READ "Leer"
+
+  #define TXT_DIALOG_SUBPROGRAM_TITLE "Subprograma"
+  #define TXT_DIALOG_SUBPROGRAM_CALL_TITLE "Llamado a subprograma"
+  #define TXT_DIALOG_SUBPROGRAM_NAME "Nombre del subprograma:"
+  #define TXT_DIALOG_DESCRIPTION "Descripción:"
+  #define TXT_DIALOG_PARAMETERS "Parámetros:"
+  #define TXT_DIALOG_ARGUMENTS "Argumentos:"
+  #define TXT_DIALOG_COND "Condición:"
+
+  #define TXT_DIALOG_ASSIGNATION_TITLE "Asignación"
+  #define TXT_DIALOG_BEGIN_TITLE "Inicio"
+  #define TXT_DIALOG_FOR_LOOP_TITLE "Ciclo Para"
+  #define TXT_DIALOG_FOR "Para:"
+  #define TXT_DIALOG_WHILE_LOOP_TITLE "Ciclo Mientras"
+  #define TXT_DIALOG_IF_TITLE "Decisión"
+  #define TXT_DIALOG_IF_TRUE_CONDITION " Condición verdadera  "
+
+  #define TXT_FREEDFD_LICENSE "FreeDFD es Software Libre, y está cubierto por la licencia  pública GNU (GPL).\n\nUsted puede tener acceso al código fuente, y puede modificarlo de acuerdo a sus necesidades si cumple con la licencia. Lo invitamos a copiar y distribuir este programa.\n\nEstamos trabajando en una nueva versión. Necesitamos ayuda para el desarrollo. Favor visitar la página (http://freedfd.freaks-unidos.net) para obtener más información sobre el desarrollo de la nueva version"
+  #define TXT_WEBPAGE_CHECK "Consulte la página web para ver la lista completa de autores y colaboradores"
+
+
+
+
+
+
+
   /* __SPANISH__ END */
   #elif 0 /* Português by Charles Guimarães Cavalcante - www.charles.art.br - e-mail@charles.art.br */
   /* __PORTUGUESE__ BEGIN */
+
+
+
+
+
+
   #define TXT_DESBORDAMIENTO "Transbordamento na operação"
   #define TXT_DESBORDAMIENTO_NEGATIVO "Transbordamento negativo na operação"
   #define TXT_LOS_TIPOS_NO_COINCIDEN "Os tipos se coincidem"
