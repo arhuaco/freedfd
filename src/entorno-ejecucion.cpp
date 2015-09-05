77b398d36d18c5ba35d358803a82870ceaecd748
Nelson Castillo <nelsoneci@gmail.com>
2015-09-05 11:15:01 -0500

    Inden

--- src/entorno-ejecucion.cpp
+++ src/entorno-ejecucion.cpp
@@ -2,86 +2,61 @@
 #include <entorno-ejecucion.h>
 #include <errores.h>
 
-void
-PilaDeTablas::Vacear ()
-{
-  while (Tope)
-    {
-      Tabla *Aux = Tope;
-      Tope = Tope->GetSig ();
-      delete Aux;
-    }
+void PilaDeTablas::Vacear() {
+  while (Tope) {
+    Tabla *Aux = Tope;
+    Tope = Tope->GetSig();
+    delete Aux;
+  }
 }
 
-Token *
-PilaDeTablas::Leer (const char *Id, unsigned *Indices, int Dim)
-{
-  if (!Tope)
-    {
-      Buzon.SetIdentificadorAsociado (Id);
-      Buzon.Error (VARIABLE_NO_EXISTE);
-      return 0;
-    }
-  return Tope->Leer (Id, Indices, Dim);
-}
-
-
-Variable *
-PilaDeTablas::Buscar (const char *Id)
-{
-  if (!Tope)
-    {
-      Buzon.SetIdentificadorAsociado (Id);
-      Buzon.Error (VARIABLE_NO_EXISTE);
-      return 0;
-    }
-  return Tope->Buscar (Id);
-}
-
-void
-PilaDeTablas::ActualizarVariables (Variable * Muestra)
-{
-  Variable *Primera = Muestra->GetPrimerPadre ();
-  bool HastaAqui = false;
-  for (Tabla * t = Tope; t && !HastaAqui; t = t->GetSig ())
-    for (Variable * v = t->GetInicio (); v; v = v->GetSig ())
-      {
-
-        if (v->GetPrimerPadre () == Primera)
-          {
-            v->SetFU (true);
-            v->SetCampo (Muestra->GetCampo ());
-            v->SetTipo (Muestra->GetCampo ()->GetTipo ());
-          }
-        if (v == Primera)
-          {
-            HastaAqui = true;
-            break;
-          }
-
-      }
-}
-
-void
-ActualizarVariables (Variable * Muestra)
-{
-  PilaDeTablas.ActualizarVariables (Muestra);
-}
-
-Token *
-EntornoEjecucion_BuscaSimbolo (const char * symbol)
-{
-  Token *Retorno = PilaDeTablas.Leer (symbol, 0, 0);
-  if (Buzon.GetHuboError ())
+Token *PilaDeTablas::Leer(const char *Id, unsigned *Indices, int Dim) {
+  if (!Tope) {
+    Buzon.SetIdentificadorAsociado(Id);
+    Buzon.Error(VARIABLE_NO_EXISTE);
     return 0;
-  Retorno->SetTipoAlmacenamiento (VARIABLE);
+  }
+  return Tope->Leer(Id, Indices, Dim);
+}
+
+Variable *PilaDeTablas::Buscar(const char *Id) {
+  if (!Tope) {
+    Buzon.SetIdentificadorAsociado(Id);
+    Buzon.Error(VARIABLE_NO_EXISTE);
+    return 0;
+  }
+  return Tope->Buscar(Id);
+}
+
+void PilaDeTablas::ActualizarVariables(Variable *Muestra) {
+  Variable *Primera = Muestra->GetPrimerPadre();
+  bool HastaAqui = false;
+  for (Tabla *t = Tope; t && !HastaAqui; t = t->GetSig())
+    for (Variable *v = t->GetInicio(); v; v = v->GetSig()) {
+      if (v->GetPrimerPadre() == Primera) {
+        v->SetFU(true);
+        v->SetCampo(Muestra->GetCampo());
+        v->SetTipo(Muestra->GetCampo()->GetTipo());
+      }
+      if (v == Primera) {
+        HastaAqui = true;
+        break;
+      }
+    }
+}
+
+void ActualizarVariables(Variable *Muestra) {
+  PilaDeTablas.ActualizarVariables(Muestra);
+}
+
+Token *EntornoEjecucion_BuscaSimbolo(const char *symbol) {
+  Token *Retorno = PilaDeTablas.Leer(symbol, 0, 0);
+  if (Buzon.GetHuboError()) return 0;
+  Retorno->SetTipoAlmacenamiento(VARIABLE);
   return Retorno;
 }
 
-Token *
-EntornoEjecucion_BuscaSimbolo (Token * UnToken)
-{
-  if (UnToken->GetTipoAlmacenamiento () == CONSTANTE)
-    return UnToken;
+Token *EntornoEjecucion_BuscaSimbolo(Token *UnToken) {
+  if (UnToken->GetTipoAlmacenamiento() == CONSTANTE) return UnToken;
   return EntornoEjecucion_BuscaSimbolo(UnToken->GetDatoStr());
 }
--- src/entorno-ejecucion.h
+++ src/entorno-ejecucion.h
@@ -4,63 +4,49 @@
 #include <tabla-simbolos.h>
 #include <token.h>
 
-class PilaDeTablas
-{
-
+class PilaDeTablas {
   Tabla *Tope;
 
-public:
-    PilaDeTablas ()
-  {
-    Tope = 0;
-  }
-  void Apilar (Tabla * UnaTabla)
-  {
-    UnaTabla->SetSig (Tope);
+ public:
+  PilaDeTablas() { Tope = 0; }
+  void Apilar(Tabla *UnaTabla) {
+    UnaTabla->SetSig(Tope);
     Tope = UnaTabla;
   }
-  void Desapilar ()
-  {
+  void Desapilar() {
     Tabla *Aux;
     Aux = Tope;
-    Tope = Tope->GetSig ();
+    Tope = Tope->GetSig();
     delete Aux;
   }
 
-  Variable *Crear (char *Id, Token * UnToken, Variable * Vengo,
-                   unsigned *Indices, int Dim)
-  {
-    return Tope->Crear (Id, UnToken, Vengo, Indices, Dim);
+  Variable *Crear(char *Id, Token *UnToken, Variable *Vengo, unsigned *Indices,
+                  int Dim) {
+    return Tope->Crear(Id, UnToken, Vengo, Indices, Dim);
   }
-  Variable *Crear (char *Id, Variable * Vengo, bool FP)
-  {
-    return Tope->Crear (Id, Vengo, FP);
+  Variable *Crear(char *Id, Variable *Vengo, bool FP) {
+    return Tope->Crear(Id, Vengo, FP);
   }
 
-  Variable *Buscar (const char *Id);
+  Variable *Buscar(const char *Id);
 
-  void AsignarValor (const char *Id, Token * UnToken, unsigned *Indices, int Dim)
-  {
-
-    Tope->AsignarValor (Id, UnToken, Indices, Dim);
+  void AsignarValor(const char *Id, Token *UnToken, unsigned *Indices,
+                    int Dim) {
+    Tope->AsignarValor(Id, UnToken, Indices, Dim);
     return;
   }
-  Tabla *GetTope ()
-  {
-    return Tope;
-  }
-  Token *Leer (const char *Id, unsigned *Indices, int Dim);
-  void ActualizarVariables (Variable *);
-  void Vacear ();
-
+  Tabla *GetTope() { return Tope; }
+  Token *Leer(const char *Id, unsigned *Indices, int Dim);
+  void ActualizarVariables(Variable *);
+  void Vacear();
 };
 
 /* Busca el valor de una variable en el entorno de ejecución actual. */
-Token * EntornoEjecucion_BuscaSimbolo (const char * symbol);
+Token *EntornoEjecucion_BuscaSimbolo(const char *symbol);
 /* Busca el valor de una variable en el entorno de ejecución actual.
  * Si el token que llega es una constante, retorna el mismo token */
-Token * EntornoEjecucion_BuscaSimbolo (Token *symbol);
+Token *EntornoEjecucion_BuscaSimbolo(Token *symbol);
 
-void ActualizarVariables (Variable *);
+void ActualizarVariables(Variable *);
 
 #endif
